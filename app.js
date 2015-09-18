60f7b26185cedf64fbb3641144f0e2bbc594d16c
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-09-18 15:39:45 -0500

    Bootstrap application

---
+++ app.js
@@ -0,0 +1,18 @@
+angular.module('myApp', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider
+            .when('/inbox', {
+                templateUrl: 'views/inbox.html',
+                controller: 'inboxCtrl',
+                controllerAs: 'inbox'
+            })
+            .when('/inbox/email/:id', {
+                templateUrl: 'views/email.html',
+                controller: 'EmailCtrl',
+                controllerAs: 'email'
+            })
+            .otherwise({
+                redirectTo: '/inbox'
+            });
+    }]);
+
