960725140836ab99443e5c5e077e99286a0d5644
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-09-18 16:33:13 -0500

    Create inbox view

--- scripts/InboxCtrl.js
+++ scripts/InboxCtrl.js
@@ -1,9 +1,9 @@
 angular.module('myApp')
-	.controller('InboxCtrl', ['$scope', 'InboxFactory', function($scope, InboxFactory) {
-		$scope.title = "This is a title";
-		InboxFactory.getMessages()
-			.success(function(data, status) {
-				console.log('The request was successful', status, data);
-				$scope.emails = data;
-			})
-	}]);
+    .controller('InboxCtrl', ['$scope', 'InboxFactory', function($scope, InboxFactory) {
+        $scope.title = "This is a title";
+        InboxFactory.getMessages()
+            .success(function(data, status) {
+                console.log('The request was successful', status, data);
+                $scope.emails = data;
+            })
+    }]);
